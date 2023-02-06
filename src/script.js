6f07c61e51aa9d47258f546c4d4e3c7f4b70bcb6
Olivia Marshall <o.marshall91@outlook.com>
2023-02-06 12:39:00 +0000

    re-input JS

--- src/script.js
+++ src/script.js
@@ -1,83 +1,84 @@
 function searchCity(city) {
-    let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiURL).then(showWeatherInfo);
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    let city = document.querySelector("input").value;
-  
-    searchCity(city);
-  }
-  
-  function showWeatherInfo(response) {
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = response.data.name;
-    
-    let description= response.data.weather[0].description;
-    let descriptionDisplay=document.querySelector("#description");
-    descriptionDisplay.innerHTML=`${description}`;
-    
-    let temperature = Math.round(response.data.main.temp);
-    let temperatureDisplay = document.querySelector("div.col.currentTemperature");
-    temperatureDisplay.innerHTML = `${temperature}°C `;
-  
-    let humidity = Math.round(response.data.main.humidity);
-    let humidityDisplay = document.querySelector("div.humidity");
-    humidityDisplay.innerHTML = `Humidity:${humidity}%`;
-  
-    let windSpeed = Math.round(response.data.wind.speed);
-    let windSpeedDisplay = document.querySelector("div.windSpeed");
-    windSpeedDisplay.innerHTML = `Wind Speed:${windSpeed}mph`;
+  let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(showWeatherInfo);
+}
 
-    let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("input").value;
+
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("form");
+
+searchForm.addEventListener("submit", handleSubmit);
+
+function showWeatherInfo(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.name;
+
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureDisplay = document.querySelector("div.col.currentTemperature");
+  temperatureDisplay.innerHTML = `${temperature}°C `;
+
+  let description = response.data.weather[0].description;
+  let descriptionDisplay = document.querySelector("#description");
+  descriptionDisplay.innerHTML = `${description}`;
+
+  let humidity = Math.round(response.data.main.humidity);
+  let humidityDisplay = document.querySelector("div.humidity");
+  humidityDisplay.innerHTML = `Humidity:${humidity}%`;
+
+  let windSpeed = Math.round(response.data.wind.speed);
+  let windSpeedDisplay = document.querySelector("div.windSpeed");
+  windSpeedDisplay.innerHTML = `Wind Speed:${windSpeed}mph`;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  }
+}
 
-  let searchForm = document.querySelector("form");
-  
-  searchForm.addEventListener("submit", handleSubmit);
-  
-  let now = new Date();
-  
-  let h6 = document.querySelector("h6");
-  
-  let date = now.getDate();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let year = now.getFullYear();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;}
-  
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[now.getDay()];
-  
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  let month = months[now.getMonth()];
-  
-  h6.innerHTML = `${day} ${date} ${month} ${year}, ${hours}:${minutes}`;
-  
+let now = new Date();
+
+let h6 = document.querySelector("h6");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let month = months[now.getMonth()];
+
+h6.innerHTML = `${day} ${date} ${month} ${year}, ${hours}:${minutes}`;
+
+searchCity("Oslo");
